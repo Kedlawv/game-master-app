960b3e38d4b342ae5f39852ce9d299c6cef8b476
Waldek <waldek.bus@gmail.com>
2024-08-13 09:42:04 +0200

    Initialize new game with confirmation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { PlayerListComponent } from './components/player-list/player-list.component';
 import {HttpClientModule} from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDialogModule} from '@angular/material/dialog';
+import { ConfirmationDialogComponent } from './confirmation-dialog/confirmation-dialog.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PlayerListComponent
+    PlayerListComponent,
+    ConfirmationDialogComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/player-list/player-list.component.ts
+++ src/app/components/player-list/player-list.component.ts
@@ -2,6 +2,8 @@ import { KeyValuePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { PlayerService, Player } from 'src/app/services/player.service';
 import {PlainWebSocketService} from '../../services/plain-websocket.service';
+import {MatDialog} from '@angular/material/dialog';
+import {ConfirmationDialogComponent} from '../../confirmation-dialog/confirmation-dialog.component';
 
 @Component({
   selector: 'app-player-list',
@@ -12,7 +14,9 @@ export class PlayerListComponent implements OnInit {
   players: { [key: string]: Player} = {};
   private playerFound: boolean = false;
 
-  constructor(private playerService: PlayerService, private webSocketService: PlainWebSocketService) {}
+  constructor(private playerService: PlayerService,
+              private webSocketService: PlainWebSocketService,
+              public dialog: MatDialog) {}
 
   ngOnInit(): void {
     this.webSocketService.onNewPlayer().subscribe((player: Player) => {
@@ -32,7 +36,30 @@ export class PlayerListComponent implements OnInit {
 
 
   startGame(): void {
-    this.webSocketService.sendMessage('start-game-gm', 'Game started!');
-    console.log('Start game event emitted');
+    const dialogRef = this.dialog.open(ConfirmationDialogComponent, {
+      width: '250px',
+      data: { title: 'Start Game', message: 'Are you sure you want to start the game?' }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.webSocketService.sendMessage('start-game-gm', 'Game started!');
+        console.log('Start game event emitted');
+      }
+    });
+  }
+
+  clearCurrentPlayers(): void {
+    const dialogRef = this.dialog.open(ConfirmationDialogComponent, {
+      width: '250px',
+      data: { title: 'Initialize New Game', message: 'Are you sure you want to clear the current players?' }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.webSocketService.sendMessage('init-new-game', '')
+        console.log('Initialising new game. Request to clear players list sent to server.')
+      }
+    });
   }
 }
